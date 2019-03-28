9ae92e6e247518551b4251c964a4893531d93731
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-03-28 18:10:39 +0100

    koniec

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-alert('Skrypt działa prawidłowo!')
+/*let name = 'Tablica'
+let table = document.querySelector('#table');
+let array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12]
+];
+fillTable(table, array);
+
+function fillTable(table, array) {
+    for (let i = 0; i < array.length; i++) {
+        let tr = document.createElement('tr');
+
+        for (let j = 0; j < array[i].length; j++) {
+            let td = document.createElement('td');
+            td.innerHTML = array[i][j];
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+}*/
+let x = 23;
+let y = 46;
+let z = 54;
+let arr = [x, y, z];
+console.log(arr);
